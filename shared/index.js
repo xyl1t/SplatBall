9aca64b135d884c305aef3128e8cf061ffd78a08
Xylit <56483075+xyl1t@users.noreply.github.com>
2023-12-25 11:56:01 +0100

    add serializer and deserializer to shared

--- shared/index.js
+++ shared/index.js
@@ -1,4 +1,11 @@
-import { Types, defineComponent } from "bitecs";
+import { Types, defineComponent, defineDeserializer, defineSerializer } from "bitecs";
 
 export const Position = defineComponent({ x: Types.f32, y: Types.f32, z: Types.f32 });
+
+const serializationConfig = [
+  Position,
+];
+
+export const serialize = defineSerializer(serializationConfig);
+export const deserialize = defineDeserializer(serializationConfig);
 
