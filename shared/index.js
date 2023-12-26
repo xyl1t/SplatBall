ea05d0b0520823bfd7cb52cb66be523a262bb67c
Xylit <56483075+xyl1t@users.noreply.github.com>
2023-12-26 10:03:29 +0100

    format index.js

--- shared/index.js
+++ shared/index.js
@@ -1,13 +1,18 @@
-import { Types, defineComponent, defineDeserializer, defineSerializer } from "bitecs";
+import {
+  Types,
+  defineComponent,
+  defineDeserializer,
+  defineSerializer,
+} from "bitecs";
 
-export const Position = defineComponent({ x: Types.f32, y: Types.f32, z: Types.f32 });
+export const Position = defineComponent({
+  x: Types.f32,
+  y: Types.f32,
+  z: Types.f32,
+});
 export const Me = defineComponent(); // defines the playing entity (the player)
 
-const serializationConfig = [
-  Position,
-  Me,
-];
+const serializationConfig = [Position, Me];
 
 export const serialize = defineSerializer(serializationConfig);
 export const deserialize = defineDeserializer(serializationConfig);
-
